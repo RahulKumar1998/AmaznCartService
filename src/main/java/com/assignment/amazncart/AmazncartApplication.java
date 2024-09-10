0c24be99a443fb05e7020d52b19f2fe33ce035e5
RahulKumar1998 <rahulkumar08.work@gmail.com>
2024-09-10 15:49:30 +0530

    initialized Spring Boot project with starter dependencies

---
+++ src/main/java/com/assignment/amazncart/AmazncartApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.amazncart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazncartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazncartApplication.class, args);
+	}
+
+}
